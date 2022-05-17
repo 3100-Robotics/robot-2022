8bf705c416749ad3106eab014569a15c949a520d
Spencer H <spenceh66@gmail.com>
2022-05-17 15:24:49 -0500

    Final Commit - Afterseason changes :-(

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,21 +4,24 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import frc.robot.Drivetrain.CurvyDrive;
-import frc.robot.Drivetrain.VelocityDrive;
-import frc.robot.Drivetrain.ZoomZoomFast;
+import frc.robot.drivetrain.CurvyDrive;
+import frc.robot.drivetrain.VelocityDrive;
+import frc.robot.drivetrain.ZoomZoomFast;
+import frc.robot.util.AxisButton;
 import frc.robot.autonomous.autoncommands.AutoAdjustHood;
 import frc.robot.autonomous.autonroutes.TestGrouping;
 import frc.robot.commands.Aim;
-import frc.robot.commands.AutonAlign;
 import frc.robot.commands.Climb;
 import frc.robot.commands.DriveLimeTurn;
 import frc.robot.commands.LimeTurn;
-import frc.robot.commands.TurretLimeTurn;
+import frc.robot.commands.VisionAlignDrivetrain;
+import frc.robot.commands.shootercommands.ShootTable;
 import frc.robot.commands.shootercommands.ToggleShooterMode;
+import frc.robot.commands.shootercommands.WaitForShooterVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
@@ -40,49 +43,10 @@ import edu.wpi.first.wpilibj2.command.button.POVButton;
  */
 public class RobotContainer {
 
- 
   private final XboxController m_driveController = new XboxController(driveControllerPort);
   private final static XboxController m_techController = new XboxController(techControllerPort);
-
-  // public final JoystickButton turnNeg = new JoystickButton(m_driveController,
-  // backButtonChannel);
-  // public final JoystickButton turnPos = new JoystickButton(m_driveController,
-  // startButtonChannel);
-  // public final JoystickButton velocityDrive = new
-  // JoystickButton(m_driveController, rightTriggerChannel);
-  public final JoystickButton limeTurn = new JoystickButton(m_driveController, xButtonChannel);
-  // private final AxisButton limeTurn = new AxisButton(m_driveController,
-  // Constants.leftTriggerChannel);
-  // public final JoystickButton limeTurn2 = new JoystickButton(m_driveController,
-  // yButtonChannel);
-  public final JoystickButton determineDistance = new JoystickButton(m_driveController, aButtonChannel);
-
-  public final JoystickButton m_deployCollector = new JoystickButton(m_techController, leftBumperChannel);
-  public final JoystickButton m_groundCollect = new JoystickButton(m_techController, aButtonChannel);
-  public final JoystickButton m_reverseGroundCollect = new JoystickButton(m_techController, yButtonChannel);
-  public final JoystickButton m_conveyorRun = new JoystickButton(m_techController, xButtonChannel);
-  public final JoystickButton m_reverseConveyorRun = new JoystickButton(m_techController, bButtonChannel);
-
-  public final JoystickButton enableCompresser = new JoystickButton(m_driveController, startButtonChannel);
-  public final JoystickButton disableCompresser = new JoystickButton(m_driveController, backButtonChannel);
-  // public final JoystickButton m_collectToShoot = new
-  // JoystickButton(m_techController, yButtonChannel);
-  // public final JoystickButton m_hoodAdjust = new
-  // JoystickButton(m_driveController, yButtonChannel);
-
-  // private final POVButton shooterFar = new POVButton(m_driveController, POVU);
-  // private final POVButton shooterNear = new POVButton(m_driveController, POVD);
-  // public final JoystickButton shooterFar = new
-  // JoystickButton(m_driveController, bButtonChannel);
-  // public final JoystickButton shooterNear = new
-  // JoystickButton(m_driveController, aButtonChannel);
-  // public final JoystickButton shooterFarther = new
-  // JoystickButton(m_driveController, yButtonChannel);
-  public final JoystickButton shoot = new JoystickButton(m_driveController, rightBumperChannel);
-  public final JoystickButton adjustShooter = new JoystickButton(m_driveController, leftBumperChannel);
-
-  // public final JoystickButton m_climberPistonToggle = new
-  // JoystickButton(m_techController, backButtonChannel);
+  private final XboxController m_rod = new XboxController(rodControllerPort);
+  public final boolean isRod = false;
 
   private final SequentialCommandGroup TestGrouping = new SequentialCommandGroup(
       new frc.robot.autonomous.autonroutes.TestGrouping(m_drive, m_shooter, m_collector));
@@ -92,6 +56,7 @@ public class RobotContainer {
       new frc.robot.autonomous.autonroutes.OneBallOffLine(m_drive, m_shooter, m_collector));
 
   SendableChooser<Command> m_chooser = new SendableChooser<>();
+  SendableChooser<Boolean> m_isRod = new SendableChooser<>();
   SendableChooser<String> m_driveType = new SendableChooser<>();
   public static SendableChooser<String> m_position = new SendableChooser<>();
 
@@ -102,25 +67,40 @@ public class RobotContainer {
     configureButtonBindings();
     new RobotCommands();
 
-    if(getDriveType() == "Curvy"){
+    // if (getDriveType() == "Curvy") {
 
-       m_drive.setDefaultCommand(
-      new CurvyDrive(m_drive, m_driveController));
-   
+    // m_drive.setDefaultCommand(
+    //     new CurvyDrive(m_drive, m_driveController));
+
+    // } else {
+
+    m_drive.setDefaultCommand(
+    new ZoomZoomFast(m_drive, () -> -m_driveController.getLeftY(),
+    () -> m_driveController.getRightX()));
+
+    // }
+
+    
+
+    if(isRod){
+
+      m_climber.setDefaultCommand(
+        new Climb(m_climber, () -> m_rod.getRawAxis(rightXAxisChannel), false));
+
     }else{
 
-      m_drive.setDefaultCommand(
-        new ZoomZoomFast(m_drive, () -> -m_driveController.getLeftY(),
-            () -> m_driveController.getRightX()));
+      m_climber.setDefaultCommand(
+    new Climb(m_climber, () -> m_techController.getRightY(), true));
 
     }
 
     
 
     // m_climber.setDefaultCommand(
-    // new Climb(m_climber, () -> m_techController.getRightY()));
+    // new Climb(m_climber, () -> m_rod.getRawAxis(leftYAxisChannel), true));
 
     m_driveType.addOption("Curvy", "Curvy");
+    m_isRod.addOption("Rod", isRod);
 
     m_chooser.addOption("TestGrouping", TestGrouping);
     m_chooser.addOption("TwoBall", TwoBall);
@@ -132,42 +112,88 @@ public class RobotContainer {
 
   private void configureButtonBindings() {
 
-    // turnPos.whileHeld(turnTurretPos);
-    // turnNeg.whileHeld(turnTurretNeg);
-    // limeTurn.whileHeld(new DriveLimeTurn(m_drive), true);
-    limeTurn.whileActiveContinuous(new LimeTurn(m_drive, () -> -m_driveController.getLeftY()));
-    // limeTurn2.whileHeld(new Aim(m_turret, m_limelight, true));
-    // velocityDrive.whileActiveContinuous(new VelocityDrive(m_drive, () ->
-    // -m_driveController.getLeftY(),
-    // () -> m_driveController.getRightX()));
-    // m_deployCollector.whenPressed(deployCollectorCommand);
-    m_deployCollector.whenPressed(deployCollectorCommand);
-    m_groundCollect.whileHeld(groundCollect);
-    // m_groundCollect.whenReleased(deployCollectorCommand);
-    m_reverseGroundCollect.whileHeld(reverseGroundCollect);
-    m_conveyorRun.whileHeld(conveyorRun);
-    // m_conveyorRun.whenReleased(deployCollectorCommand);
-    m_reverseConveyorRun.whileHeld(reverseConveyorRun);
-    // m_collectToShoot.whileHeld(collectToShoot);
-    // shooterNear.whenPressed(new AutoAdjustHood(30));
-    // shooterFar.whenPressed(new AutoAdjustHood(0));
-    // shooterFarther.whenPressed(new AutoAdjustHood(110));
-    determineDistance.whenPressed(findDistance);
+    // final JoystickButton limeTurn = new JoystickButton(m_driveController, xButtonChannel);
+    // limeTurn.whileActiveContinuous(new LimeTurn(m_drive, () -> -m_driveController.getLeftY()));
 
-    shoot.whileHeld(shootSpee);
-    adjustShooter.whenPressed(new ToggleShooterMode(m_shooter));
+    // final JoystickButton limeTurn2 = new JoystickButton(m_driveController, yButtonChannel);
+    // limeTurn2.whileActiveContinuous(new VisionAlignDrivetrain(m_limelight, m_drive));
 
+    // final JoystickButton determineDistance = new JoystickButton(m_driveController, aButtonChannel);
+    // determineDistance.whenPressed(printDistance);
+
+
+    final JoystickButton enableCompresser = new JoystickButton(m_driveController, startButtonChannel);
     enableCompresser.whenPressed(compressorEnable);
+    final JoystickButton disableCompresser = new JoystickButton(m_driveController, backButtonChannel);
     disableCompresser.whenPressed(compressorDisable);
 
-    // m_climberPistonToggle.whenPressed(toggleClimberSolenoids);
+    final JoystickButton shoot = new JoystickButton(m_driveController, rightBumperChannel);
+    //shoot.whileHeld(new ShootTable(m_shooter, m_turret));
+    shoot.whileHeld(shootSpee);
+    // final JoystickButton adjustShooter = new JoystickButton(m_driveController, leftBumperChannel);
+    // adjustShooter.whenPressed(new ToggleShooterMode(m_shooter));
+
+    if(isRod){
+
+      final JoystickButton m_deployCollector = new JoystickButton(m_rod, leftBumperChannel);
+      m_deployCollector.whenPressed(deployCollectorCommand);
+      final JoystickButton m_groundCollect = new JoystickButton(m_rod, aButtonChannel);
+      m_groundCollect.whileHeld(groundCollect);
+      final JoystickButton m_reverseGroundCollect = new JoystickButton(m_rod, yButtonChannel);
+      m_reverseGroundCollect.whileHeld(reverseGroundCollect);
+      final JoystickButton m_conveyorRun = new JoystickButton(m_rod, xButtonChannel);
+      m_conveyorRun.whileHeld(conveyorRun);
+      final JoystickButton m_reverseConveyorRun = new JoystickButton(m_rod, bButtonChannel);
+      m_reverseConveyorRun.whileHeld(reverseConveyorRun);
+      final JoystickButton m_reverseShooter = new JoystickButton(m_rod, rightBumperChannel);
+      m_reverseShooter.whileHeld(reverseShooter);
+      final AxisButton m_climberPistonToggle = new AxisButton(m_rod, leftTriggerChannel);
+      m_climberPistonToggle.whileActiveOnce(toggleClimberSolenoids);
+
+      final POVButton m_reset = new POVButton(m_rod, POVU);
+       m_reset.whenPressed(resetClimberSensors);
+
+    }else{
+
+      final JoystickButton m_deployCollector = new JoystickButton(m_techController, leftBumperChannel);
+      m_deployCollector.whenPressed(deployCollectorCommand);
+      final JoystickButton m_groundCollect = new JoystickButton(m_techController, aButtonChannel);
+      m_groundCollect.whileHeld(groundCollect);
+      final JoystickButton m_reverseGroundCollect = new JoystickButton(m_techController, yButtonChannel);
+      m_reverseGroundCollect.whileHeld(reverseGroundCollect);
+      final JoystickButton m_conveyorRun = new JoystickButton(m_techController, xButtonChannel);
+      m_conveyorRun.whileHeld(conveyorRun);
+      final JoystickButton m_reverseConveyorRun = new JoystickButton(m_techController, bButtonChannel);
+      m_reverseConveyorRun.whileHeld(reverseConveyorRun);
+      final JoystickButton m_reverseShooter = new JoystickButton(m_techController, rightBumperChannel);
+      m_reverseShooter.whileHeld(reverseShooter);
+  
+     
+  
+      final AxisButton m_climberPistonToggle = new AxisButton(m_techController, leftTriggerChannel);
+      m_climberPistonToggle.whileActiveOnce(toggleClimberSolenoids);
+
+      final POVButton m_reset = new POVButton(m_techController, POVU);
+       m_reset.whenPressed(resetClimberSensors);
+    }
+
+  
+
+   // shoot.whileHeld(new WaitForShooterVelocity(m_shooter, ShooterConstants.DIST_TO_RPM_TABLE.get(1.0)));
+   //shoot.whileHeld(shootSpee);
+   //shoot.whileHeld(new ShootTable(m_shooter, m_turret));
+    
+
+    
 
   }
 
-  public String getDriveType(){
+  public String getDriveType() {
     return m_driveType.getSelected();
   }
-
+  public Boolean getTechControllerType(){
+    return m_isRod.getSelected();
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
